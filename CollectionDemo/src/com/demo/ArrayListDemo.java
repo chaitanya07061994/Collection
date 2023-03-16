6176ca820020bbd52859f462226e222719e42efe
chaitanya07061994 <chaitanyakapse99@gmail.com>
2023-03-16 13:43:59 +0530

    Added ArraylistDemo File

---
+++ CollectionDemo/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		List <String> list = Arrays.asList("Nayan","Rohan","Abhik","Priyay");
+		
+		System.out.println(list);
+		
+
+	}
+
+}
